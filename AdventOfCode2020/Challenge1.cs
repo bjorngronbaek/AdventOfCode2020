f6739d57eb5db1cce103a06f4a655bdba4bd268c
Bjørn Grønbæk <github@gronbaek.net>
2020-12-05 12:38:42 +0100

    Day 1, first challenge solved

---
+++ AdventOfCode2020/Challenge1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class Challenge1
+    {
+        public int RunFirst()
+        {
+            var allLines = File.ReadAllLines("challenges/1_1.txt");
+            var values = Array.ConvertAll<string, int>(allLines, int.Parse);
+            Array.Sort(values);
+            for (int i = 0; i < values.Length; i++)
+            {
+                for (int j = i+1; j < values.Length; j++)
+                {
+                    var first = values[i];
+                    var second = values[j];
+                    if (first + second == 2020)
+                    {
+                        Console.WriteLine($"Values are {first} and {second}");
+                        return first * second;
+                    }
+
+                    if (first + second > 2020)
+                    {
+                        break;
+                    }
+                }
+            }
+            
+            throw new Exception("No such value");
+        }
+    }
+}
---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent Of Code 2020");
+            var runFirst = new Challenge1().RunFirst();
+            Console.WriteLine(runFirst);
+        }
+    }
+}
