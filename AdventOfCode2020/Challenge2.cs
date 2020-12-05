843a0cccef7cf64f44706e9d6af31de664b4279e
Bjørn Grønbæk <github@gronbaek.net>
2020-12-05 13:08:31 +0100

    Day 2, first challenge solved

---
+++ AdventOfCode2020/Challenge2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class Challenge2
+    {
+        public int RunFirst()
+        {
+            var validCounter = 0;
+            var allLines = File.ReadAllLines("challenges/2_1.txt");
+            foreach (var line in allLines)
+            {
+                var split = line.Split(':');
+                var policy = ParsePolicy(split[0]);
+
+                var charCount = split[1].Count(character => character == policy.Item3);
+                if (policy.Item1 <= charCount && charCount <= policy.Item2) validCounter++;
+            }
+
+            return validCounter;
+        }
+
+        private (int, int, char) ParsePolicy(string policyString)
+        {
+            var split = policyString.Split(' ');
+            var bounds = split[0].Split('-');
+            return (int.Parse(bounds[0]), int.Parse(bounds[1]), Convert.ToChar(split[1]));
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -7,8 +7,9 @@ namespace AdventOfCode2020
         static void Main(string[] args)
         {
             Console.WriteLine("Advent Of Code 2020");
-            Console.WriteLine(new Challenge1().RunFirst());
-            Console.WriteLine(new Challenge1().RunSecond());
+            //Console.WriteLine(new Challenge1().RunFirst());
+            //Console.WriteLine(new Challenge1().RunSecond());
+            Console.WriteLine(new Challenge2().RunFirst());
         }
     }
 }
